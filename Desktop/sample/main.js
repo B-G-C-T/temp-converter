3dc97d10e098f1a10dbe509c039ca6a81664c971
Your Name <you@example.com>
2024-01-19 12:48:31 -0800

    sample1

---
+++ Desktop/sample/main.js
@@ -0,0 +1,7 @@
+const colors = ["purple", "green", "orange", "rgba(123,586,666)", "3333"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    console.log(document.body);
+})
